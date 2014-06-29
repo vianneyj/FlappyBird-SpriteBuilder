889c6ca36f8378a1db5eedd1a2d127ca528297c6
Vianney Jayasinha <vianney@Vianneys-MacBook-Pro.local>
2014-06-29 11:06:21 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,17 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
 }
-
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    [character flap];
+}
 // put new methods here
 
 @end
